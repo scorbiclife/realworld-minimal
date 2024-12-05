a91fd0b8633058c2ff3bc1e687b138b85974e0a1
scorbiclife <scorbiclife+git@gmail.com>
2024-12-05 10:03:36 +0900

    setup: migrate to liquibase

--- src/db/migration-3-rollback.sql
+++
@@ -1,3 +0,0 @@
-alter table article_tag drop foreign key article_tag_fk_tag;
-alter table tag modify column tag_id int not null;
-alter table article_tag add constraint article_tag_fk_tag foreign key (tag_id) references tag(tag_id);
--- src/db/migration-4-rollback.sql
+++
@@ -1,5 +0,0 @@
-alter table article_tag drop foreign key article_tag_fk_article;
-alter table `comment` drop foreign key `comment_fk_article`;
-alter table article modify column article_id int not null;
-alter table `comment` add constraint `comment_fk_article` foreign key (article_id) references article(article_id);
-alter table article_tag add constraint article_tag_fk_article foreign key (article_id) references article(article_id);
